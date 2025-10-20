02c9bfbf6aeadacfee8205ed24a967a887d4fb5e
Emily <emily-l93@hotmail.com>
2025-10-20 15:12:05 +0300

    Create version.php

---
+++ version.php
@@ -0,0 +1,9 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_autocontent';
+$plugin->version   = 2025102000;       // Plugin version (YYYYMMDDXX)
+$plugin->requires  = 2024042200;       // Requires Moodle 4.5 or later
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->release   = '1.0 (for Moodle 4.5+)';
